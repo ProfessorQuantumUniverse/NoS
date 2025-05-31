e4fac34d0d21f0b004bcb43975c35f2c021d603e
ProfessorQuantumUniverse <lorenzobaymueller@gmail.com>
2025-05-31 20:51:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // WICHTIG: Trage hier die URL deiner bereitgestellten Google Apps Script Web-App ein!
-const GOOGLE_SCRIPT_URL = 'DEINE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HIER_EINTRAGEN';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzQ5DbQrbKo8sZPCtNgNiRYQmrnUpcPFY2P6WstnqSGdOgwvEtRV3gXjZXxHpSU1XBc/exec';
 
 // Beispiel:
 // const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/DEIN_SCRIPT_ID/exec';
