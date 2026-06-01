962bbf7030b78c8647957e9a11497c9033cabc96
makerLab314 <makerlab.fffm@gmail.com>
2026-06-01 18:46:33 +0200

    fix: Update GOOGLE_SCRIPT_URL to the correct endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // WICHTIG: Trage hier die URL deiner bereitgestellten Google Apps Script Web-App ein!
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxCD1Z5bNCImDuLxAyHi4FwcUcJ_IMuTmsIMvkBD9ziu7ZEoVtBtht9lbWqCBZ-fm7ZaA/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwsnczjouHcrwkuNPGjQE2SkUivlb1NjxXx3xTqR-FVBB76OuafwmJdsFShCnirx4U6UA/exec';
 
 // Beispiel:
 // const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/DEIN_SCRIPT_ID/exec';
